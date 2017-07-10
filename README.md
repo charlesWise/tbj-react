d220a0b41e4985a6eee88639dca7a80680093b5a
lianqin <chenrunsheng@lianqin360.comchenrunsheng@lianqin360.comchenrunsheng@lianqin360.comchenrunsheng@lianqin360.comchenrunsheng@lianqin360.com>
2017-07-10 16:39:40 +0800

    init tbj-react

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# tbj-react
+## 技术栈：
+react + react-router + redux + immutable + less + ES6/7 + webpack + fetch
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {Component, PropTypes} from 'react';
+import ReactDOM, {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import route from './Router/Route'; //路由配置
+import store from './Redux/Store/Store';
+import './Config/Config.js';//引入默认配置
+
+import './Style/common';
+import './Style/head';
+import './Style/index';
+import './Style/chooseProducts';
+import './Style/helpCenter';
+import './Style/saleRecord';
+import './Style/allDeposit';
+import './Style/applyDeposit';
+import './Style/applyRecord';
+
+
+store.subscribe(() => { //监听state变化
+    console.log(store.getState())
+});
+
+render(
+    <Provider store={store}>
+        {route}
+    </Provider>,
+    document.body.appendChild(document.createElement('div'))
+);
